ac24f50d76ad3148ec7d678d73a570520098c678
RedL <52675202+RedL-GNU-GPL@users.noreply.github.com>
2019-07-21 22:45:55 +0300

    Create install.sh

---
+++ debian/0.02B/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install gcc g++ gpp glade libgtk-3-dev pkg-config libglade2-dev -y
+sudo sh -c "echo 'PATH=\$PATH:/var/redl/' >> ~/.bashrc"
+sudo sh -c "echo 'export PATH' >> ~/.bashrc"
+cp -r redl /var/
+
+g++ /var/redl/redl.cpp -o /var/redl/redl
