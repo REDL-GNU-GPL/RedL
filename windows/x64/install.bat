73ee7a21f9bb088f5743989ace4307d2d0bd9053
RedL <52675202+RedL-GNU-GPL@users.noreply.github.com>
2019-07-22 22:41:27 +0300

    Update install.bat

--- windows/x64/install.bat
+++ windows/x64/install.bat
@@ -12,3 +12,4 @@ set FolderToAdd=C:\Users\%UserName%\AppData\Roaming\redl\gcc\bin\
 For /f "tokens=2*" %%a In ('Reg.exe query "%key%" /v Path^|Find "Path"') do set CurPath=%%~b
 reg.exe add "%Key%" /v Path /t REG_EXPAND_SZ /d "%CurPath%;%FolderToAdd%
 
+pause
