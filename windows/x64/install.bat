0e5616a0cad5d27d9cd7d307ada4f9c30fc210f4
RedL <52675202+RedL-GNU-GPL@users.noreply.github.com>
2019-07-22 22:48:55 +0300

    Update install.bat

--- windows/x64/install.bat
+++ windows/x64/install.bat
@@ -1,6 +1,8 @@
 @echo off
 
 cd %~dp0
+move redl %appdata%
+
 set Key=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
 set FolderToAdd=C:\Users\%UserName%\AppData\Roaming\redl\
 For /f "tokens=2*" %%a In ('Reg.exe query "%key%" /v Path^|Find "Path"') do set CurPath=%%~b
