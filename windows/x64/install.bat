6631975b2cee1872efb5526647d6b9880b8c539a
RedL <52675202+RedL-GNU-GPL@users.noreply.github.com>
2019-07-22 23:15:37 +0300

    Update install.bat

--- windows/x64/install.bat
+++ windows/x64/install.bat
@@ -4,7 +4,7 @@ cd %~dp0
 move redl %appdata%
 
 set Key=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
-set FolderToAdd=C:\Users\%UserName%\AppData\Roaming\redl\
+set FolderToAdd=C:\MinGW\bin
 For /f "tokens=2*" %%a In ('Reg.exe query "%key%" /v Path^|Find "Path"') do set CurPath=%%~b
 reg.exe add "%Key%" /v Path /t REG_EXPAND_SZ /d "%CurPath%;%FolderToAdd%
 
