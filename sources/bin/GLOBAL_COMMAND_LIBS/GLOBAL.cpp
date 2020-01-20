e26d70608e10183f17b630c1dd5b479a639ec92f
RedL <52675202+REDL-GNU-GPL@users.noreply.github.com>
2020-01-20 20:42:58 +0300

    1

--- sources/bin/GLOBAL_COMMAND_LIBS/GLOBAL.cpp
+++
@@ -1,6 +0,0 @@
-if((libs.find("vprint", 0) + 1)) { 
-	#include "GLOBAL_VPRINT.cpp"
-}
-if((libs.find("math", 0) + 1)) { 
-	#include "GLOBAL_MATH.cpp"
-}
--- sources/bin/vprint/while/do.cpp
+++
@@ -1,7 +0,0 @@
-if(timeint == 0) {
-	if (newer[x] == 'd') {
-		if (newer[x+1] == 'o') {
-			out << "do" << '\n';		
-		}
-	}
-}
